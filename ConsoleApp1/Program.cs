707ee4eaa28ce5a78863e4fefe30b0d3f520781f
Stephen I Killgore <hmm45ss.killgore@gmail.com>
2020-02-28 02:41:33 -0600

    new

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+            Console.WriteLine("First Name: ");
+            a = Console.ReadLine();
+            Console.WriteLine("Last Name: ");
+            b = Console.ReadLine();
+            Console.WriteLine("Hello, " + a + " " + b + " and welcome to your first Program in C#!");
+        }
+    }
+}

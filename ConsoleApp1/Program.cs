395b7e2f1e6b2551402b8933acfc4263f7af0b35
Stephen I Killgore <hmm45ss.killgore@gmail.com>
2020-02-28 03:14:51 -0600

    loop added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,12 +6,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+           int loop = 1;
+           while ( loop == 1 ) { 
             string a, b;
+            a:
             Console.WriteLine("First Name: ");
             a = Console.ReadLine();
             Console.WriteLine("Last Name: ");
             b = Console.ReadLine();
             Console.WriteLine("Hello, " + a + " " + b + " and welcome to your first Program in C#!");
+            string choice;
+            Console.WriteLine("Continue?");
+            choice = Console.ReadLine();
+            if (choice == "continue")
+            {
+                goto a;
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+           }
         }
     }
 }

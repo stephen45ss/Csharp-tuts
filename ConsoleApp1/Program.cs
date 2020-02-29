83c1761f3cf34b321ca55cfe08ead807c6a79300
Stephen I Killgore <hmm45ss.killgore@gmail.com>
2020-02-29 03:17:56 -0600

    new version

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,54 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
-        {
-           int loop = 1;
-           while ( loop == 1 ) { 
-            string a, b;
-            a:
-            Console.WriteLine("First Name: ");
+        {       a:
+		string mname;    
+		string filepath = "hello.txt";
+	    int loop = 1;
+            while ( loop == 1 ) { 
+            string a, b, c;
+            Console.WriteLine("Male or female?");
+	    c = Console.ReadLine();
+	    Console.WriteLine("First Name: ");
             a = Console.ReadLine();
             Console.WriteLine("Last Name: ");
             b = Console.ReadLine();
-            Console.WriteLine("Hello, " + a + " " + b + " and welcome to your first Program in C#!");
-            string choice;
+	    if (c == "female" || c == "f" || c == "Female"){
+	    Console.WriteLine("Maiden Name: ");
+	    mname = Console.ReadLine();
+	    goto c;
+	    }
+	    else { goto c; }
+	c:
+	    Console.WriteLine("The name " + a + " " + b + " was added to our database!");
+            if (!File.Exists(filepath)){
+	    using (StreamWriter file = File.CreateText(filepath))
+	    {file.WriteLine("Names: "); 
+	    if (c == "female" || c == "f" || c == "Female"){
+	    file.WriteLine(a + " " + b + ":" + mname); 
+	    goto b;}
+	    else {
+	    file.WriteLine(a + " " + b + " "); 
+	    goto b;}
+	    }}
+		    using (StreamWriter file = File.AppendText(filepath))
+	    { 
+		     if (c == "female" || c == "f" || c == "Female"){
+			     file.WriteLine(a + " " + b + ":" + mname);
+			      goto b;}
+		     else {
+		    file.WriteLine(a + " " + b + " "); 
+		     goto b; } }
+	b:
+	    string choice;
             Console.WriteLine("Continue?");
             choice = Console.ReadLine();
-            if (choice == "continue")
+            if (choice == "continue" | choice == "c")
             {
                 goto a;
             }
@@ -27,6 +57,4 @@ namespace ConsoleApp1
                 Environment.Exit(0);
             }
            }
-        }
-    }
-}
+	}}}
